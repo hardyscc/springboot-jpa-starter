3e89c2bf06b3fb538547c2f967a68d225e0732ab
Hardys <hardyscc@gmail.com>
2022-04-07 11:31:15 +0800

    chore: cleanup

--- src/main/java/com/example/sjs/controller/HealthController.java
+++ src/main/java/com/example/sjs/controller/HealthController.java
@@ -1,14 +1,13 @@
 package com.example.sjs.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
-@Slf4j
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class HealthController {
